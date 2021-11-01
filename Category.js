745b76a25a5ddddb79904a727c56129e70448d35
Josue Reyes <90571004+jereyes96@users.noreply.github.com>
2021-10-31 21:51:32 -0500

    updated files

---
+++ Category.js
@@ -0,0 +1,29 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+     id: {
+       type: DataTypes, INTEGER,
+       primaryKey: true,
+       notNull: true,
+       autoincrement: true,
+     },
+     categoryname: {
+       type: DataTypes.STRING,
+       notNull: true,
+     },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
